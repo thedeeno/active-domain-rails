4533eeda1495de4917feda7fc438dfcb663bb4f9
Dane O'Connor <dane.oconnor@gmail.com>
2015-11-16 09:48:42 -0500

    generate gem

---
+++ lib/active/domain/rails.rb
@@ -0,0 +1,9 @@
+require "active/domain/rails/version"
+
+module Active
+  module Domain
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/active/domain/rails/version.rb
@@ -0,0 +1,7 @@
+module Active
+  module Domain
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
